2540289963c57001bfb86e78fa5388226ada93be
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-11 19:46:38 +0900

    feat:project start

---
+++ Navibook/src/main/java/KNU/Navibook/NavibookApplication.java
@@ -0,0 +1,13 @@
+package KNU.Navibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavibookApplication.class, args);
+	}
+
+}
---
+++ Navibook/src/test/java/KNU/Navibook/NavibookApplicationTests.java
@@ -0,0 +1,13 @@
+package KNU.Navibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavibookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
