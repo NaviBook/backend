1dd8a72227d5eac0695a640407084755830a3f2b
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-29 22:46:13 +0900

    make bookinfocontroller

---
+++ Navibook/src/main/java/KNU/Navibook/server/controller/BookInfoController.java
@@ -0,0 +1,31 @@
+package KNU.Navibook.server.controller;
+
+import KNU.Navibook.server.domain.BookInfo;
+import KNU.Navibook.server.service.BookInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@RequestMapping("/api/bookinfo")
+@Controller
+public class BookInfoController {
+    @Autowired
+    BookInfoService bookInfoService;
+
+    @GetMapping("/returnAllBookInfos")
+    @ResponseBody
+    public List<BookInfo> example() {
+        return bookInfoService.findAll(); //스프링이 자동으로 JSON타입으로 객체를 반환해서 전달한다.
+    }
+    @GetMapping("/searchbookinfo")
+    @ResponseBody
+    public List<BookInfo> searchBookInfoByName(@RequestParam("bookname") String name) {
+        System.out.println(name);
+        return bookInfoService.findBybooknameContaining(name); //스프링이 자동으로 JSON타입으로 객체를 반환해서 전달한다.
+    }
+}
