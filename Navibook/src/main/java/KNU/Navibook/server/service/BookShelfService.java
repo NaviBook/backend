8ac62383c949bda8f836d6b8877cea43012783ec
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-30 21:21:16 +0900

    create bookShelfService

--- Navibook/src/main/java/KNU/Navibook/server/service/BookShelfService.java
+++ Navibook/src/main/java/KNU/Navibook/server/service/BookShelfService.java
@@ -1,4 +1,22 @@
 package KNU.Navibook.server.service;
 
+import KNU.Navibook.server.domain.BookInfo;
+import KNU.Navibook.server.domain.BookShelf;
+import KNU.Navibook.server.repository.BookShelfRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookShelfService {
+    @Autowired
+    private BookShelfRepository bookShelfRepository;
+
+    public List<BookShelf> findAll(){
+        return bookShelfRepository.findAll();
+    }
+    public List<BookShelf> findByLibraryFloor(String floor){
+        return bookShelfRepository.findBylibraryFloor(floor);
+    }
 }
---
+++ Navibook/src/test/java/KNU/Navibook/server/service/BookShelfTest.java
@@ -0,0 +1,33 @@
+package KNU.Navibook.server.service;
+
+import KNU.Navibook.server.domain.Book;
+import KNU.Navibook.server.domain.BookShelf;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class BookShelfTest {
+    @Autowired
+    BookShelfService bookShelfService;
+
+    @Test
+    public void 책장전체조회(){
+        List<BookShelf> bookShelves=bookShelfService.findAll();
+        for(BookShelf bookShelf:bookShelves){
+            System.out.println(bookShelf.getId());
+        }
+    }
+
+    @Test
+    public void 층별책장조회(){
+        String floor="1F";
+        List<BookShelf> bookShelves=bookShelfService.findByLibraryFloor(floor);
+        for (BookShelf bookShelf:bookShelves){
+            System.out.println(bookShelf.getId());
+        }
+    }
+
+}
