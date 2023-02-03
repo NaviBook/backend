9499ceefd3745b8a1ee3aafc0dab04c268caa084
tfer2442 <tfer2442@gmail.com>
2023-02-03 19:19:38 +0900

    edit bookController bookadd

--- src/main/java/KNU/Navibook/server/controller/BookController.java
+++ src/main/java/KNU/Navibook/server/controller/BookController.java
@@ -33,16 +33,23 @@ public class BookController {
         return bookService.findBook(bookInfoService.findOne(infoId));
     }
 
+//        1. 이미 존재하는 book_id 값이라면 등록 거부(오류 메시지)
+//        2. bookinfo_id 값이 존재하지 않으면 등록 거부
     @PostMapping("/api/book/add")
     @ResponseBody
     public Book bookAdd(@RequestBody Map<String, Object> requestData) {
-        Integer bookId= (Integer)requestData.get("bookId");
-        Integer bookInfoId= (Integer)requestData.get("bookInfoId");
+        Integer tmp1 = (Integer) requestData.get("bookId"); // Long으로 한 번에 못 받아옴
+        Integer tmp2 = (Integer) requestData.get("bookInfoId");
+        Long bookId = tmp1.longValue();
+        Long bookInfoId = tmp2.longValue();
 
-        Book book = bookService.findOne((bookId.longValue()));
-        BookInfo bookinfo = bookInfoService.findOne((bookInfoId.longValue()));
 
+        Book book = new Book();
+        book.setId(bookId);
+
+        BookInfo bookinfo = bookInfoService.findOne(bookInfoId);
         book.setBookInfo(bookinfo);
+
         bookService.saveBook(book);
 
         return book;
