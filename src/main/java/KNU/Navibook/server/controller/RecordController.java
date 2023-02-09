56412f357bc8ef40b1cd28e3d7467ba339927c1b
tfer2442 <tfer2442@gmail.com>
2023-02-09 23:30:07 +0900

    edit userRecordAPI

--- src/main/java/KNU/Navibook/server/controller/RecordController.java
+++ src/main/java/KNU/Navibook/server/controller/RecordController.java
@@ -20,16 +20,26 @@ public class RecordController {
     @GetMapping("/api/record/user/{userId}")
     @ResponseBody
     public List<Record> userRecord(@PathVariable("userId") String userId,
-                                   @RequestParam("page") Integer page)//, @RequestParam("orederBy") String orderBy)
+                                   @RequestParam("page") Integer page, @RequestParam("orderBy") String orderBy)
     {
         User user = userService.findOne(userId);
         List<Record> records = recordService.findRecordByUser(user);
         List<Record> pageRecords = new ArrayList<>();
 
-//        Comparator<Record> cp = Comparator.comparing(Record::getGiveDate);
-//        Collections.sort(records, cp);
+        if (orderBy.equals("book")){ // bookName 오름차순 정렬
+            Comparator<Record> bs = Comparator.comparing(a -> a.getBook().getBookInfo().getBookName());
+            Collections.sort(records, bs);
+        }
+        else if (orderBy.equals("takeDate")){ // TakeDate 오름차순 정렬
+            Comparator<Record> ts = Comparator.comparing(Record::getTakeDate);
+            Collections.sort(records, ts);
+        }
+        else if (orderBy.equals("giveDate")){ // GiveDate 오름차순 정렬
+            Comparator<Record> gs = Comparator.comparing(Record::getGiveDate);
+            Collections.sort(records, gs);
+        }
 
-        for(int i = (page*10)-10; records != null && records.size() >= i && i <= page*10; i++){
+        for(int i = (page*10)-10; (records != null) && (records.size() > i) && (i < page*10); i++){
             pageRecords.add(records.get(i));
         }
 
