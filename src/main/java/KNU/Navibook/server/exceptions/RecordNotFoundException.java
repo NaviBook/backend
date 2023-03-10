8667ad22e993d3a3cc16d61f57f2915777e7851c
tfer2442 <tfer2442@gmail.com>
2023-03-10 19:43:00 +0900

    feature/RecordException

---
+++ src/main/java/KNU/Navibook/server/exceptions/RecordNotFoundException.java
@@ -0,0 +1,17 @@
+package KNU.Navibook.server.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RecordNotFoundException extends RuntimeException {
+    public RecordNotFoundException(String message){
+        super(message);
+    }
+}
